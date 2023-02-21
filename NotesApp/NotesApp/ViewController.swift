5be703b60627727e16ab7e1f7b3d375c74286fbf
A Kasarin <>
2023-02-21 19:04:23 +0300

    Project init

---
+++ NotesApp/NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Apple Macbook Pro 13 on 21.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
