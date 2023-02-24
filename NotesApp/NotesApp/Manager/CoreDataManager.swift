bb59b197be57d1873c8181c10a115f21ca4d2aca
A Kasarin <>
2023-02-24 13:43:14 +0300

    Create manager

---
+++ NotesApp/NotesApp/Manager/CoreDataManager.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataManager.swift
+//  NotesApp
+//
+//  Created by Apple Macbook Pro 13 on 24.02.23.
+//
+import CoreData
+import UIKit
+
+final class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private init() {}
+}
+
+extension CoreDataManager {
+    // Loading
+    func loadFolders(
+        with request: NSFetchRequest<Folder> = Folder.fetchRequest(),
+        predicate: String? = nil
+    ) -> [Folder]? {
+        if let predicate = predicate {
+            request.predicate = NSPredicate(format: "name CONTAINS", predicate)
+        }
+        do {
+            return try context.fetch(request)
+        } catch  {
+            print("Error loading folders: \(error)")
+            return nil
+        }
+    }
+    
+    // Saving
+    func saveContext() {
+        do {
+            try context.save()
+        } catch {
+            print("Error while saving context: \(error)")
+        }
+    }
+}
