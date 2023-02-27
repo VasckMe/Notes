09dc83bcff9aa302a6341cf70266c05d7f72427d
A Kasarin <>
2023-02-27 19:47:25 +0300

    Add tableView on FolderViewController

--- NotesApp/NotesApp/Controller/FolderViewController.swift
+++ NotesApp/NotesApp/Controller/FolderViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 final class FolderViewController: UIViewController {
-    @IBOutlet private weak var tableView: UITableView!
+    
+    var folders = [Folder]()
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
     @IBOutlet private weak var searchBar: UISearchBar!
     
     @IBAction private func createFolderAction(_ sender: UIBarButtonItem) {
@@ -22,14 +30,43 @@ final class FolderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        loadFolders()
     }
 }
 
+extension FolderViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FolderTableViewCell") else {
+            return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = folders[indexPath.row].name
+        cell.detailTextLabel?.text = "number"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folders.count
+    }
+}
+
+extension FolderViewController: UITableViewDelegate {
+    
+}
 
 // MARK: - Private API
 
 private extension FolderViewController {
     func setupUI() {
         title = "Folders"
+    }
+    
+    func loadFolders() {
+        guard let folders = CoreDataManager.shared.loadFolders() else {
+            return
+        }
+        
+        self.folders = folders
     }
 }
